9e5d1cc7317ea82425f905dfe7ca150b2bfb99b0
ohenekwabena <adjeiasarejesse@gmail.com>
2024-09-13 02:19:48 +0000

    set up handlebars for dynamic page render on server

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,22 @@
-function getMessages (req, res){
-    res.send({
-        id: 1, 
-        text: 'Hello World!',
-        author: 'John Doe'       
-   })
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const messages = [
+         "Elon Kweku Musk", 
+          "Albert Kojo Einstein",
+         "Marie Adwoa Curie",
+          "Charles Papa Darwin",
+    ]
+function getMessages(req, res) {
+    console.log(__filename, __dirname);
+    
+    res.render("messages", {
+        messages
+    })
+
 }
 
 export {
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from "express";
-import { getMessages } from "./controllers/messages.controller.js";
+import path from "path";
 import friendsRouter from "./routes/friends.route.js";
 import messagesRouter from "./routes/messages.route.js";
+import { fileURLToPath } from "url";
+
 
 
 const app = express();
 const PORT = 3000;
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 
 app.use((req, res, next) => {
@@ -18,10 +26,15 @@ app.use((req, res, next) => {
     console.log(`Time: ${delta}ms`);
 })
 
+app.use("/site", express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index", {
+        title: "Ice Skating",
+        caption: "What a wonderful day!",
+    })
 });
 
 app.use("/friends", friendsRouter);
